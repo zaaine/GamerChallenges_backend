2b6fedf24d8f339f3f2dcfc5a4180b758d681e2b
David <synlike@gmail.com>
2025-09-17 10:24:25 +0200

    feat: create auth route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
-import express from 'express'
-import cors from"cors"
-import { config } from '../config.js'
+import express from "express";
+import cors from "cors";
+import { config } from "../config.js";
+import { router } from "./router.js";
 
+export const app = express();
 
-export const app = express()
+app.use(cors({ origin: config.server.allowedOrigins }));
+app.disable("x-powered-by");
 
-
-app.use(cors({origin: config.server.allowedOrigins}))
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use("/api", router);
