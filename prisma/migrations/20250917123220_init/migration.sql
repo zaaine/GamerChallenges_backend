8cf3fdd773d2faaef4484f657c60b5bbbcba84a8
David <synlike@gmail.com>
2025-09-17 14:36:04 +0200

    schema prisma complete with all models

---
+++ prisma/migrations/20250917123220_init/migration.sql
@@ -0,0 +1,101 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('member', 'admin');
+
+-- CreateTable
+CREATE TABLE "public"."user" (
+    "user_id" SERIAL NOT NULL,
+    "pseudo" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "public"."Role" NOT NULL DEFAULT 'member',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."game" (
+    "game_id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "game_pkey" PRIMARY KEY ("game_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."challenge" (
+    "challenge_id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "rules" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "game_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "challenge_pkey" PRIMARY KEY ("challenge_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."entry" (
+    "entry_id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "video_url" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "challenge_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "entry_pkey" PRIMARY KEY ("entry_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."vote_user_challenge" (
+    "user_id" INTEGER NOT NULL,
+    "challenge_id" INTEGER NOT NULL,
+
+    CONSTRAINT "vote_user_challenge_pkey" PRIMARY KEY ("user_id","challenge_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."vote_user_entry" (
+    "user_id" INTEGER NOT NULL,
+    "entry_id" INTEGER NOT NULL,
+
+    CONSTRAINT "vote_user_entry_pkey" PRIMARY KEY ("user_id","entry_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "public"."user"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."challenge" ADD CONSTRAINT "challenge_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."challenge" ADD CONSTRAINT "challenge_game_id_fkey" FOREIGN KEY ("game_id") REFERENCES "public"."game"("game_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."entry" ADD CONSTRAINT "entry_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."entry" ADD CONSTRAINT "entry_challenge_id_fkey" FOREIGN KEY ("challenge_id") REFERENCES "public"."challenge"("challenge_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."vote_user_challenge" ADD CONSTRAINT "vote_user_challenge_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."vote_user_challenge" ADD CONSTRAINT "vote_user_challenge_challenge_id_fkey" FOREIGN KEY ("challenge_id") REFERENCES "public"."challenge"("challenge_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."vote_user_entry" ADD CONSTRAINT "vote_user_entry_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."vote_user_entry" ADD CONSTRAINT "vote_user_entry_entry_id_fkey" FOREIGN KEY ("entry_id") REFERENCES "public"."entry"("entry_id") ON DELETE RESTRICT ON UPDATE CASCADE;
