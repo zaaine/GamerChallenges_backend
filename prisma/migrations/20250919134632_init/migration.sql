d761399eef835f33f9f09c31b5ee1f6341ae4489
David <synlike@gmail.com>
2025-09-24 11:32:31 +0200

    fix: add unique constraint on pseudo and rework register in authController

--- prisma/migrations/20250919134632_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[title]` on the table `game` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "game_title_key" ON "public"."game"("title");
