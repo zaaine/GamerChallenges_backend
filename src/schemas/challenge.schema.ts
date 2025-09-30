9fdacf75ffc5d69ee0cf2881fc128526957da810
Raphael-Aldaz <raphael.aldaz@gmail.com>
2025-09-30 13:22:43 +0200

    fix: fixing zod validation and verifytoken controller

---
+++ src/schemas/challenge.schema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod"
+
+export const challengeSchema = z.object({
+  title: z.string().min(3, "Le titre doit contenir au moins 3 caractères"),
+  description: z
+    .string()
+    .min(10, "La description doit contenir au moins 10 caractères")
+    .refine((val) => !/<script.*?>.*?<\/script>/i.test(val), {
+      message: "La description ne doit pas contenir de balises <script>",
+    }),
+  rules: z
+    .string()
+    .min(10, "Les règles doivent contenir au moins 10 caractères")
+    .refine((val) => !/<script.*?>.*?<\/script>/i.test(val), {
+      message: "Les règles ne doivent pas contenir de balises <script>",
+    }),
+  game_title: z.string().optional(),
+  game_id: z.number().int().positive().optional(),
+})
+
+export type ChallengeInput = z.infer<typeof challengeSchema>
