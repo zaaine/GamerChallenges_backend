b205a2110413507cbe2ddbf5bb38339fe71e6293
David <synlike@gmail.com>
2025-10-01 12:23:50 +0200

    fix: comment swagger for future fix

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,9 @@
 import { Router } from "express"
+// import path from "node:path"
+// import { dirname } from "path"
+// import swaggerJSDoc from "swagger-jsdoc"
+// import swaggerUi from "swagger-ui-express"
+// import { fileURLToPath } from "url"
 import authRoutes from "./routes/auth.routes.js"
 import challengeRoutes from "./routes/challenge.route.js"
 import entryRoutes from "./routes/entry.routes.js"
@@ -11,6 +16,55 @@ router.use("/challenges", challengeRoutes)
 router.use("/entries", entryRoutes)
 
 router.use("/games", gameRoutes)
+
+// const spec = swaggerJSDoc({
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Gamer-Challenges",
+//       version: "1.0.0",
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:5000/api",
+//         description: "Development server",
+//       },
+//     ],
+//     components: {
+//       schemas: {
+//         Challenge: {
+//           type: "object",
+//           properties: {
+//             id: {
+//               type: "integer",
+//               description: "ID unique du défi",
+//             },
+//             title: {
+//               type: "string",
+//               description: "Titre du défi",
+//             },
+//             rules: {
+//               type: "string",
+//               desciption: "Regles du défi",
+//             },
+//             description: {
+//               type: "string",
+//               description: "Description du défi",
+//             },
+//             createdAt: {
+//               type: "string",
+//               format: "date-time",
+//               description: "Date de création",
+//             },
+//           },
+//         },
+//       },
+//     },
+//   },
+//   apis: [path.join(__dirname, "../src/docs/openapi.yml")],
+// })
+
+// router.use("/docs", swaggerUi.serve, swaggerUi.setup(spec))
 
 //Default 404
 router.use((req, res) => {
